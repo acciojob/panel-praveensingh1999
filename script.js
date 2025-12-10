1f011d22da35e0abd8a3c5ca45e5b49734365639
Praveen Singh <57175335+praveensingh1999@users.noreply.github.com>
2025-12-10 22:43:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach(panel => {
+  panel.addEventListener('click', () => {
+    removeActiveClass();
+    panel.classList.add('active');
+  });
+});
+
+function removeActiveClass() {
+  panels.forEach(panel => panel.classList.remove('active'));
+}
